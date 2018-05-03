b96be60f36ceb6818538c25b6bf88a4368306a67
webfunny <webfunny@sina.com>
2018-05-03 22:27:26 +0800

    api

--- js/takeout_mall/config.js
+++ js/takeout_mall/config.js
@@ -4,7 +4,7 @@
 
 // var host = "apitest.ipaotui.com"
 var host = "api.nanhuaren.cn/ajax/wxapp";
-var uniacid = 6;
+var uniacid = 8;
 const debug = wx.getStorageSync('debug')
 if (debug) {
   host = "api.nanhuaren.cn/ajax/wxapp"
